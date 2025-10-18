e5c99dc2a503dc24826360355664799a8b730b79
Husein81 <husseinnasrallah2002@gmail.com>
2025-10-18 12:04:25 +0300

    feat:implement mono repo structure

---
+++ apps/client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@repo/ui";
+
+export default function Home() {
+  return (
+    <div className={"container bg-background"}>
+      <Button>click</Button>
+    </div>
+  );
+}
